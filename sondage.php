ac14e2247641da5a3d21fa5b56c777d079ef2e2b
Sacha Galeuzzi <sacha.galeuzzi@edu.devinci.fr>
2020-09-30 18:46:47 +0200

    test

---
+++ sondage.php
@@ -0,0 +1,30 @@
+<?php
+include 'inc/header.php'
+?>
+
+<main>
+
+<section class="page_sondage">
+
+    <div class="div_sondage">
+        <img src="img/preview_koh_lanta.jpg" alt="">
+    </div>
+
+    <div class="div_sondage">
+        <p class="date_sondage">Émission du 27/09</p>
+        <div class="question_sondage">
+            <p>À l’issu du l’épreuve d’immunité, Quel sera l’équipe gagnante  ? </p>
+        </div>
+        <section class="reponses">
+            <div class="choix_reponses"> <p>Équipe Rouge</p> <div class="bouton_choix"></div></div>
+            <div class="choix_reponses"><p>Équipe Bleu</p> <div class="bouton_choix"></div></div>
+        </section>
+        <button id="envoyer">Envoyer</button>
+
+    </div>
+    
+
+</section>
+
+
+</main>
